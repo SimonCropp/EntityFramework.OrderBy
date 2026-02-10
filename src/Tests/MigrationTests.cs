90ecdb24ca203564ea7cca2e8f8e2c5b0a545d38
Simon Cropp <simon.cropp@gmail.com>
2026-02-10 13:03:37 +1100

    MigrationTests

---
+++ src/Tests/MigrationTests.cs
@@ -0,0 +1,85 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+[TestFixture]
+public class MigrationTests
+{
+    static DbContextOptions<TestDbContext> CreateOptions()
+    {
+        var builder = new DbContextOptionsBuilder<TestDbContext>();
+        builder.UseDefaultOrderBy();
+        builder.UseSqlServer("Server=.;Database=Test;Trusted_Connection=True");
+        return builder.Options;
+    }
+
+    static List<CreateIndexOperation> GetDefaultOrderIndexOperations()
+    {
+        using var context = new TestDbContext(CreateOptions());
+        _ = context.Model;
+
+        var differ = context.GetService<IMigrationsModelDiffer>();
+        var designTimeModel = context.GetService<IDesignTimeModel>().Model;
+        var operations = differ.GetDifferences(null, designTimeModel.GetRelationalModel());
+
+        return operations
+            .OfType<CreateIndexOperation>()
+            .Where(_ => _.Name.Contains("DefaultOrder"))
+            .OrderBy(_ => _.Name)
+            .ToList();
+    }
+
+    [Test]
+    public void ProducesCreateIndexOperations()
+    {
+        var indexOps = GetDefaultOrderIndexOperations();
+        Assert.That(indexOps, Has.Count.EqualTo(8));
+        var indexNames = indexOps.Select(_ => _.Name).ToList();
+        Assert.That(indexNames, Does.Contain("IX_TestEntity_DefaultOrder"));
+        Assert.That(indexNames, Does.Contain("IX_AnotherEntity_DefaultOrder"));
+        Assert.That(indexNames, Does.Contain("IX_EntityWithMultipleOrderings_DefaultOrder"));
+        Assert.That(indexNames, Does.Contain("IX_Department_DefaultOrder"));
+        Assert.That(indexNames, Does.Contain("IX_Employee_DefaultOrder"));
+        Assert.That(indexNames, Does.Contain("IX_EmployeeTask_DefaultOrder"));
+        Assert.That(indexNames, Does.Contain("IX_BaseEntity_DefaultOrder"));
+        Assert.That(indexNames, Does.Contain("IX_DerivedEntityB_DefaultOrder"));
+    }
+
+    [Test]
+    public void MultiColumnIndex_HasCorrectColumns()
+    {
+        var indexOps = GetDefaultOrderIndexOperations();
+        var multiColumnOp = indexOps.Single(_ => _.Name == "IX_EntityWithMultipleOrderings_DefaultOrder");
+        Assert.That(multiColumnOp.Columns, Is.EqualTo(["Category", "Priority", "Name"]));
+    }
+
+    [TestCase("IX_TestEntity_DefaultOrder", "CreatedDate")]
+    [TestCase("IX_AnotherEntity_DefaultOrder", "Name")]
+    [TestCase("IX_Department_DefaultOrder", "DisplayOrder")]
+    [TestCase("IX_Employee_DefaultOrder", "HireDate")]
+    [TestCase("IX_EmployeeTask_DefaultOrder", "Priority")]
+    [TestCase("IX_BaseEntity_DefaultOrder", "SortOrder")]
+    [TestCase("IX_DerivedEntityB_DefaultOrder", "Name")]
+    public void SingleColumnIndex_HasCorrectColumn(string indexName, string expectedColumn)
+    {
+        var indexOps = GetDefaultOrderIndexOperations();
+        var op = indexOps.Single(_ => _.Name == indexName);
+        Assert.That(op.Columns, Is.EqualTo([expectedColumn]));
+    }
+
+    [Test]
+    public async Task DefaultOrderOperations()
+    {
+        var indexOps = GetDefaultOrderIndexOperations();
+        var snapshot = indexOps.Select(_ => new
+        {
+            _.Name,
+            _.Table,
+            _.Columns,
+            _.IsUnique,
+            _.IsDescending
+        });
+        await Verify(snapshot);
+    }
+}
--- src/Tests/TestDbContext.cs
+++ src/Tests/TestDbContext.cs
@@ -12,19 +12,19 @@ public class TestDbContext(DbContextOptions<TestDbContext> options) :
     public DbSet<DerivedEntityA> DerivedEntitiesA => Set<DerivedEntityA>();
     public DbSet<DerivedEntityB> DerivedEntitiesB => Set<DerivedEntityB>();
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder model)
     {
-        base.OnModelCreating(modelBuilder);
+        base.OnModelCreating(model);
 
         // Configure default ordering using fluent API
-        modelBuilder.Entity<TestEntity>()
+        model.Entity<TestEntity>()
             .OrderByDescending(_ => _.CreatedDate);
 
-        modelBuilder.Entity<AnotherEntity>()
+        model.Entity<AnotherEntity>()
             .OrderBy(_ => _.Name);
 
         // Multiple orderings: Category ASC, then Priority DESC, then Name ASC
-        modelBuilder.Entity<EntityWithMultipleOrderings>()
+        model.Entity<EntityWithMultipleOrderings>()
             .OrderBy(_ => _.Category)
             .ThenByDescending(_ => _.Priority)
             .ThenBy(_ => _.Name);
@@ -32,44 +32,44 @@ public class TestDbContext(DbContextOptions<TestDbContext> options) :
         // EntityWithoutDefaultOrder has no default ordering configured
 
         // Configure Department-Employee relationship
-        modelBuilder.Entity<Department>()
+        model.Entity<Department>()
             .HasMany(_ => _.Employees)
             .WithOne(_ => _.Department)
             .HasForeignKey(_ => _.DepartmentId)
             .IsRequired();
 
         // Default ordering for Department: DisplayOrder ascending
-        modelBuilder.Entity<Department>()
+        model.Entity<Department>()
             .OrderBy(_ => _.DisplayOrder);
 
         // Default ordering for Employee: HireDate descending (newest first)
-        modelBuilder.Entity<Employee>()
+        model.Entity<Employee>()
             .OrderByDescending(_ => _.HireDate);
 
         // Configure Employee-EmployeeTask relationship
-        modelBuilder.Entity<EmployeeTask>()
+        model.Entity<EmployeeTask>()
             .HasOne(_ => _.Employee)
             .WithMany(_ => _.Tasks)
             .HasForeignKey(_ => _.EmployeeId)
             .IsRequired();
 
         // Default ordering for EmployeeTask: Priority ascending
-        modelBuilder.Entity<EmployeeTask>()
+        model.Entity<EmployeeTask>()
             .OrderBy(_ => _.Priority);
 
         // Configure TPH inheritance for BaseEntity hierarchy
-        modelBuilder.Entity<BaseEntity>()
+        model.Entity<BaseEntity>()
             .HasDiscriminator<string>("Discriminator")
             .HasValue<BaseEntity>("Base")
             .HasValue<DerivedEntityA>("DerivedA")
             .HasValue<DerivedEntityB>("DerivedB");
 
         // Configure default ordering on base entity only - should inherit to derived types
-        modelBuilder.Entity<BaseEntity>()
+        model.Entity<BaseEntity>()
             .OrderBy(_ => _.SortOrder);
 
         // DerivedEntityB has its own explicit ordering that should take precedence over base
-        modelBuilder.Entity<DerivedEntityB>()
+        model.Entity<DerivedEntityB>()
             .OrderByDescending(_ => _.Name);
     }
 }
